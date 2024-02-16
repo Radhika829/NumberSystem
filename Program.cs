67d02227edab076b7ec606d5e93a3430d4749fa0
Radhika <radhikasn06@gmail.com>
2024-02-16 09:45:37 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JenkinsThree;
+
+Console.WriteLine("welcome to numbersystem");
+Console.WriteLine( "even numbers are ");
+NumberSystem.EvenNum();
+Console.WriteLine( "odd numbers are");
+NumberSystem.OddNum();
+
